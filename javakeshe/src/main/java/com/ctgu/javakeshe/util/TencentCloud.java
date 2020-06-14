497b6da5e353552481a56cd5249ef5eba7ac5acf
rekkles7 <361808700@qq.com>
2020-06-14 13:53:51 +0800

    mybatis 等 环境的初始化

---
+++ javakeshe/src/main/java/com/ctgu/javakeshe/util/TencentCloud.java
@@ -0,0 +1,30 @@
+package com.ctgu.javakeshe.util;
+
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.model.PutObjectRequest;
+import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.region.Region;
+
+import java.io.File;
+import java.util.UUID;
+
+public class TencentCloud {
+    private static String secretId = "AKIDDQOp8CMjHchVigsP8QU6RWL1a0FHMqQq";
+    private static String secretKey = "OFqOgmb9TXKrXwsZzED2jdIRJut88tUy";
+    private static final String localHost = "https://bucket-1259702869.cos.ap-chengdu.myqcloud.com";
+    public static String Upload(String localPackge,String localFilePath){
+        COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
+        Region region = new Region("ap-chengdu");
+        ClientConfig clientConfig = new ClientConfig(region);
+        COSClient cosClient = new COSClient(cred, clientConfig);
+        File localFile = new File(localFilePath);
+        String bucketName = "bucket-1259702869";
+        String key = localPackge+"/"+ UUID.randomUUID()+localFile.getName().substring(localFile.getName().lastIndexOf("."));
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, localFile);
+        PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest);
+        return localHost+"/"+key;
+    }
+}
