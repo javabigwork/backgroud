6bc1b53744444e97b43e8a901f79f9ed21f327b7
huzhijiewarma <798284949@qq.com>
2020-06-14 20:41:59 +0800

    login

--- javakeshe/src/main/java/com/ctgu/javakeshe/controller/UserController.java
+++ javakeshe/src/main/java/com/ctgu/javakeshe/controller/UserController.java
@@ -1,10 +1,71 @@
 package com.ctgu.javakeshe.controller;
 
+import com.ctgu.javakeshe.entity.Book;
+import com.ctgu.javakeshe.entity.User;
+import com.ctgu.javakeshe.filter.AjaxResult;
+import com.ctgu.javakeshe.service.BookService;
+import com.ctgu.javakeshe.service.UserService;
+import com.ctgu.javakeshe.util.jsontomapUtil;
+import net.sf.json.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
-@RequestMapping("qmx")
+@RequestMapping("user")
+@RestController
 public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @RequestMapping("/login")
+    public AjaxResult login(String code, String APPID, String APPSecret){
+        StringBuilder url = new StringBuilder("https://api.weixin.qq.com/sns/jscode2session?");
+        url.append("appid=");//appid设置
+        url.append(APPID);
+        url.append("&secret=");//secret设置
+        url.append(APPSecret);
+        url.append("&js_code=");//code设置
+        url.append(code);
+        url.append("&grant_type=authorization_code");
+        Map<String, Object> map = null;
+        try {
+            HttpClient client = HttpClientBuilder.create().build();//构建一个Client
+            HttpGet get = new HttpGet(url.toString());    //构建一个GET请求
+            HttpResponse response = client.execute(get);//提交GET请求
+            HttpEntity result = response.getEntity();//拿到返回的HttpResponse的"实体"
+            String content = EntityUtils.toString(result);
+            System.out.println(content);//打印返回的信息
+            JSONObject res = JSONObject.fromObject(content);//把信息封装为json
+            //把信息封装到map
+            map = jsontomapUtil.parseJSON2Map(res);//这个小工具的代码在下面
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String key = "openid";
+        String openId = (String) map.get(key);
+        User user = new User();
+        user = userService.findById(openId);
+        if(user == null){
+            user.setOpenId(openId);
+            userService.creatUser(user);
+        }else {
+            userService.saveUser(user);
+        }
+        return AjaxResult.success("成功",map);
+    }
+
 
 }
