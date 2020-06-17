5c94e277553afc663ee357d1856b20fb365a3ef4
zy <1356186879@qq.com>
2020-06-17 20:48:31 +0800

    over

--- javakeshe/src/main/java/com/ctgu/javakeshe/JavakesheApplication.java
+++ javakeshe/src/main/java/com/ctgu/javakeshe/JavakesheApplication.java
@@ -1,5 +1,7 @@
 package com.ctgu.javakeshe;
 
+import com.ctgu.javakeshe.dao.CollectDao;
+import com.ctgu.javakeshe.entity.Collect;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.ctgu.javakeshe.dao")
 public class JavakesheApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavakesheApplication.class, args);
+    public static void main(String[] args) { SpringApplication.run(JavakesheApplication.class, args);
     }
 
 }
--- javakeshe/src/main/java/com/ctgu/javakeshe/service/impl/CollectServiceImpl.java
+++ javakeshe/src/main/java/com/ctgu/javakeshe/service/impl/CollectServiceImpl.java
@@ -3,10 +3,12 @@ package com.ctgu.javakeshe.service.impl;
 import com.ctgu.javakeshe.dao.CollectDao;
 import com.ctgu.javakeshe.entity.Collect;
 
+import com.ctgu.javakeshe.entity.CollectDTO;
 import com.ctgu.javakeshe.service.CollectService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,16 +23,18 @@ public class CollectServiceImpl implements CollectService {
     @Resource
     private CollectDao collectDao;
     @Override
-    public List<Collect> selectAllByOpen_id(String openId) {
+    public List<CollectDTO> selectAllByOpen_id(String openId) {
         return collectDao.selectAllByOpen_id(openId);
     }
 
     @Override
-    public Collect addCollect(String openId, String isbn) {
-        Map<String,String> collectMap=new HashMap<>();
-        collectMap.put("openId",openId);
-        collectMap.put("isbn",isbn);
-        return collectDao.addCollect(collectMap);
+    public void addCollect(String openId, String isbn) {
+            if(collectDao.selectIfCollect(openId,isbn)==null){
+                //获取当前日期和时间存储到now中
+                Timestamp time = new Timestamp(new java.util.Date().getTime());
+                String date = time.toString().substring(0, 19);
+                collectDao.addCollect(openId,isbn,date);
+            }
     }
 
     @Override
